bc4302f453fb0d2d3c6af2c8393fd04ec10895ca
Giuseppe Persiano <giuper@gmail.com>
2022-09-01 18:51:10 +0200

    05

--- Code/05-AtomicGroups/atomicSwap.py
+++ Code/05-AtomicGroups/atomicSwap.py
@@ -1,9 +1,6 @@
 import sys
-from algosdk.v2client import algod
-from algosdk import account, mnemonic
-from algosdk.future import transaction
-from algosdk.future.transaction import AssetTransferTxn
-from utilities import wait_for_confirmation, getClient
+from algosdk.future.transaction import AssetTransferTxn, PaymentTxn, write_to_file,calculate_group_id
+from utilities import wait_for_confirmation, getClient, getSKAddr
 from utilityAsset import print_asset_holding
 
 
@@ -11,41 +8,36 @@ def swap(account1,account2,assetId,directory):
 
     algodClient=getClient(directory)
     params=algodClient.suggested_params()
-    with open(account1,'r') as f:
-        mnemo1=f.read()
-        pk1=mnemonic.to_public_key(mnemo1)
-        sk1=mnemonic.to_private_key(mnemo1)
 
-    with open(account2,'r') as f:
-        mnemo2=f.read()
-        pk2=mnemonic.to_public_key(mnemo2)
-        sk2=mnemonic.to_private_key(mnemo2)
+    sk1,pk1=getSKAddr(account1)
+    sk2,pk2=getSKAddr(account2)
+
 
 ##account2 pays account1 1Algo 
-    txn0=transaction.PaymentTxn(
+    txn0=PaymentTxn(
         sender=pk2,sp=params,receiver=pk1,amt=1_000_000)
-    transaction.write_to_file([txn0],"A2toA1.utnx")
+    write_to_file([txn0],"A2toA1.utnx")
 
 
 ##account1 transfers 4 unit of the asset to account2 
-    txn1 = transaction.AssetTransferTxn(
+    txn1=AssetTransferTxn(
         sender=pk1,sp=params,receiver=pk2,amt=4,index=assetId)
-    transaction.write_to_file([txn1],"A1toA2.utnx")
+    write_to_file([txn1],"A1toA2.utnx")
 
 ##create the group
-    gid=transaction.calculate_group_id([txn0, txn1])
+    gid=calculate_group_id([txn0, txn1])
 
 #account2 signs the payment transaction txn0
     txn0.group=gid
-    transaction.write_to_file([txn0],"A2toA1withGID.utnx")
+    write_to_file([txn0],"A2toA1withGID.utnx")
     stxn0=txn0.sign(sk2)
-    transaction.write_to_file([stxn0],"A2toA1withGID.stnx")
+    write_to_file([stxn0],"A2toA1withGID.stnx")
 
 #account 1 signs the asset transfer
     txn1.group=gid
-    transaction.write_to_file([txn1],"A1toA2withGID.utnx")
+    write_to_file([txn1],"A1toA2withGID.utnx")
     stxn1=txn1.sign(sk1)
-    transaction.write_to_file([stxn1],"A1toA2withGID.stnx")
+    write_to_file([stxn1],"A1toA2withGID.stnx")
 
     print("Asset holding before the transaction")
     print("Account 1:      ",pk1)
@@ -71,7 +63,7 @@ def swap(account1,account2,assetId,directory):
 
 if __name__=="__main__":
     if (len(sys.argv)!=5):
-        print("Usage: "+sys.argv[0]+" <account1> <account2> <assetId> <directory>")
+        print("Usage: python "+sys.argv[0]+" <account1> <account2> <assetId> <directory>")
         exit()
 
     account1=sys.argv[1]
